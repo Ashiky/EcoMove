85627b28d6b2fb494b205e6facb6d600d50fb4f3
Suzanne <suzanne.gcvs@gmail.com>
2022-06-17 15:07:07 +0200

    Nouvelle view

---
+++ EcoMove/Map.swift
@@ -0,0 +1,20 @@
+//
+//  Map.swift
+//  EcoMove
+//
+//  Created by apprenant70 on 17/06/2022.
+//
+
+import SwiftUI
+
+struct Map: View {
+    var body: some View {
+        Text("Salut")
+    }
+}
+
+struct Map_Previews: PreviewProvider {
+    static var previews: some View {
+        Map()
+    }
+}
