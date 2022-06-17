85047f34a8679d0187370565f91f3b298f13c0b0
AlyTristan <tristan.aly@gmail.com>
2022-06-17 21:10:27 +0200

    DetailPayment and ButtonPayment

---
+++ EcoMove/RewardView.swift
@@ -0,0 +1,75 @@
+//
+//  RewardView.swift
+//  EcoMove
+//
+//  Created by Tristan Aly on 17/06/2022.
+//
+
+import SwiftUI
+
+struct RewardView: View {
+    
+    @State var choose = 0
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            TopScreen()
+                .edgesIgnoringSafeArea(.top)
+            Picker("test", selection: $choose, content: {
+                Text("Dons").tag(0)
+                Text("Virements").tag(1)
+                Text("Bons d'achats").tag(2)
+            }).pickerStyle(.segmented)
+                .padding(.horizontal)
+               
+            
+            if choose == 0 {
+                ScrollView{
+                VStack {
+                    ForEach(donations, id: \.name){ dons in
+                        NavigationLink{
+//                            DetailView des dons
+                        } label: {
+//                            ScrollView des dons
+                        }
+                
+                        }
+                    }
+                }
+            } else if choose == 2 {
+                ScrollView{
+                    VStack {
+                        ForEach(purchase, id: \.name){ bons in
+                            NavigationLink{
+                                DetailBonAchat(bonpurchase: bons)
+                            } label: {
+                                ScrollBonAchat(bonpurchase: bons)
+                            }
+                    }
+                }
+                }
+                
+            } else {
+                ScrollView{
+                    VStack {
+                        ForEach(payments , id: \.name){ pay in
+                            NavigationLink{
+                                DetailPayment(payment: pay)
+                            } label: {
+                               ScrollPayment(payment: pay)
+                            }
+                    }
+                }
+                }
+            }
+            
+        }.navigationBarHidden(true)
+    }
+}
+
+struct RewardView_Previews: PreviewProvider {
+    static var previews: some View {
+        RewardView()
+    }
+}
