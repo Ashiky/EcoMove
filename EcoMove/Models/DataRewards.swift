5616c2217c25e468a91eba9754fe5b64b649572b
AlyTristan <tristan.aly@gmail.com>
2022-06-18 08:18:00 +0200

    Files in group better organization

--- EcoMove/Models/DataRewards.swift
+++ EcoMove/Models/DataRewards.swift
@@ -18,18 +18,19 @@ struct GetInfo{
 
 let payments = [GetInfo(image: "mountains", name: "Virements", icone: "moneybag", price: 20000, subtitle: "Virement à partir de 20€" , descriptif: "")]
                 
- let purchase = [GetInfo(image: "woman", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "Bon d'achat de 5 euros", descriptif: "Grand texte"),
+ let purchase = [
+    GetInfo(image: "woman", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "Bon d'achat de 5 euros", descriptif: "Grand texte"),
                  GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "Bon d'achat de 5 euros", descriptif: "Grand texte"),
                  GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "Bon d'achat de 5 euros", descriptif: "Grand texte"),
                  GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "Bon d'achat de 5 euros", descriptif: "Grand texte"),
-                 GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "Bon d'achat de 5 euros", descriptif: "Grand texte")]
+                 GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "Bon d'achat de 5 euros", descriptif: "Grand texte")
+ ]
                 
 let donations = [
     GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "virement à partir de 20€",descriptif: "Bon d'achat de 5 euros"),
                     GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "virement à partir de 20€",descriptif: "Bon d'achat de 5 euros"),
                     GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "virement à partir de 20€",descriptif: "Bon d'achat de 5 euros"),
                     GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "virement à partir de 20€",descriptif: "Bon d'achat de 5 euros"),
-                    GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "virement à partir de 20€",descriptif: "Bon d'achat de 5 euros")
-]
+                    GetInfo(image: "decathlon", name: "Decatlhon", icone: "decathlon", price: 3000, subtitle: "virement à partir de 20€",descriptif: "Bon d'achat de 5 euros")]
 
 
---
+++ EcoMove/Views/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  EcoMove
+//
+//  Created by Tristan Aly on 17/06/2022.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
