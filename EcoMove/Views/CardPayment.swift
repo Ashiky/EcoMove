a6ac07a62a8b8cea4c755856e1094b70ddc2ea80
AlyTristan <tristan.aly@gmail.com>
2022-06-18 11:53:00 +0200

    Screen Payment's coming  and Picker on HistoryView

--- EcoMove/Views/CardPayment.swift
+++ EcoMove/Views/CardPayment.swift
@@ -1,5 +1,5 @@
 //
-//  HistoryTravel.swift
+//  CardPayment.swift
 //  EcoMove
 //
 //  Created by Tristan Aly on 18/06/2022.
@@ -7,20 +7,20 @@
 
 import SwiftUI
 
-struct HistoryTravel: View {
+struct CardPayment: View {
     var body: some View {
         VStack{
             HStack(spacing: 24){
-                Image(systemName: "leaf.arrow.triangle.circlepath")
+                Image(systemName: "millsign.square.fill")
                     .font(.system(size: 40))
                     .foregroundColor(Color("DarkGreen"))
                 VStack(alignment: .leading,spacing: 5){
-                Text("Trajet économisé")
+                Text("Virement de 20€")
                         .font(.title3)
-                    Text("8h30")
+                    Text("12h30")
                         .font(.subheadline)
                 }
-                Text("40 \(Image(systemName: "millsign.square.fill"))")
+                Text("20 \(Image(systemName: "dollarsign.circle.fill"))")
                     .font(.system(size: 30))
                     .foregroundColor(Color("DarkGreen"))
             }
@@ -29,11 +29,12 @@ struct HistoryTravel: View {
         .background(RoundedRectangle(cornerRadius: 8)
             .fill(.white)
             .shadow(color: Color("DarkGreen"), radius: 5, x: 2, y: 3))
+       
     }
 }
 
-struct HistoryTravel_Previews: PreviewProvider {
+struct CardPayment_Previews: PreviewProvider {
     static var previews: some View {
-        HistoryTravel()
+        CardPayment()
     }
 }
